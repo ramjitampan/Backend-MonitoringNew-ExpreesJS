0bbf88a830cff0fcfd69e6ddb25b17138b818713
ramjitampan <junfarisramji@gmail.com>
2026-09-23 07:37:17 +0700

    Fix: add no-cache middleware for all /api routes

--- src/index.js
+++ src/index.js
@@ -28,6 +28,15 @@ app.use(limiter);
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true, limit: "1mb" }));
 
+app.use("/api", (req, res, next) => {
+  res.set({
+    "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
+    Pragma: "no-cache",
+    Expires: "0",
+  });
+  next();
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/pegawai", authenticate, pegawaiRoutes);
 app.use("/api/kendaraan", authenticate, kendaraanRoutes);
